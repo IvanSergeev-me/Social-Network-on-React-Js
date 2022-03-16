e6b2ca869a4b51a6bfa4902bb3b75dda87675778
Ivan Sergeev <inovovanya@gmail.com>
2022-03-16 16:44:47 +0300

    build v1

---
+++ precache-manifest.ad1a505a2b9f0bb790cb868ee6f23977.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4721c06ac9eedd09072a49715fe7f525",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e120003451b2625f90ea",
+    "url": "/static/css/3.f57c9351.chunk.css"
+  },
+  {
+    "revision": "25b0ff69fedfc0031f75",
+    "url": "/static/css/main.44449ee6.chunk.css"
+  },
+  {
+    "revision": "6474864320bea2521fec",
+    "url": "/static/js/2.ee6ffd4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ee6ffd4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e120003451b2625f90ea",
+    "url": "/static/js/3.9454cb5c.chunk.js"
+  },
+  {
+    "revision": "51c181d505bb24e24700",
+    "url": "/static/js/4.6ce2abd5.chunk.js"
+  },
+  {
+    "revision": "f8bbb37d0b76e5389d90",
+    "url": "/static/js/5.83b7fd6b.chunk.js"
+  },
+  {
+    "revision": "05c89674a59ce851ebe7",
+    "url": "/static/js/6.807d60c4.chunk.js"
+  },
+  {
+    "revision": "25b0ff69fedfc0031f75",
+    "url": "/static/js/main.787c5b3e.chunk.js"
+  },
+  {
+    "revision": "3869ed7f25d66e9efa5d",
+    "url": "/static/js/runtime-main.b521d188.js"
+  },
+  {
+    "revision": "f8e109b5158c2d37585c7a698c50338f",
+    "url": "/static/media/home.f8e109b5.svg"
+  },
+  {
+    "revision": "ffc99b0cab67225b5de1cc7a4f3ca4a4",
+    "url": "/static/media/preloader.ffc99b0c.svg"
+  }
+]);
